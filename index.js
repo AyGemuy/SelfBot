6d2d70bf6002d65da5cdbc90e690ac139ba42b62
Ay Gemuy 24 <91945119+AyGemuy@users.noreply.github.com>
2021-12-20 08:05:44 +0800

    SelfBot
    SelfBot

--- index.js
+++ index.js
@@ -16930,7 +16930,30 @@ alpha.sendMessage('status@broadcast', buffer, MessageType.image, {quoted: mek, c
 reply2(`Sukses upload image:\n${teksyy}`)
 break
 
-
+case 'nobg':
+if ((isMedia && !mek.message.videoMessage || isQuotedImage) && args.length == 0) {
+var imgbb = require('imgbb-uploader')
+var edit2_ = JSON.parse(JSON.stringify(mek).replace('quotedM', 'm')).message.extendedTextMessage.contextInfo
+var edit3_ = await alpha.downloadAndSaveMediaMessage(edit2_, `./media/${sender}.png`)
+let edit4_ = await imgbb("f7864144fe0b1fc22bd5f9a3f24397c7", edit3_)
+titid = await fetchJson(`https://nekobot.xyz/api/imagegen?type=${command}&url=${edit4_.display_url}`, {method: 'get'})
+ling = `${titid.message}`
+request.post(
+  {
+    url: 'https://api.slazzer.com/v2.0/remove_image_background',
+    formData: {source_image_file: fs.createReadStream(`${ling}`),},
+    headers: {"API-KEY": 'eecd31eaf03f43d2bdbd86c4e350243d',},
+    encoding: null,
+  },
+  function (error, response, body) {
+    if(error){ console.log(error); return;}
+    if(response.statusCode != 200){ console.log(body.toString('utf8')); return; }
+    fs.writeFileSync('no-bg.png', body);
+    alpha.sendMessage(from, fs.readFileSync('./no-bg.png'), image, {quoted: mek })
+  }
+);
+}
+break
 
 //Ends
 default:
