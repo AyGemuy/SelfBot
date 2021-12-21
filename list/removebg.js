7015582cfb61b263f2d719f32bc7622922f8edea
Ay Gemuy 24 <91945119+AyGemuy@users.noreply.github.com>
2021-12-21 19:26:00 +0800

    SelfBot
    SelfBot

---
+++ list/removebg.js
@@ -0,0 +1,19 @@
+
+function remove_bg(Url) {
+const formData = new FormData();
+formData.append('size', 'auto');
+formData.append('image_url', `${Url}`);
+
+axios({
+  method: 'post',
+  url: 'https://api.remove.bg/v1.0/removebg',
+  data: formData,
+  responseType: 'arraybuffer',
+  headers: {
+    ...formData.getHeaders(),
+    'X-Api-Key': 'JPrUhazLmnrPbDbZBJgK22m6',
+  },
+  encoding: null
+})
+}
+module.exports = { remove_bg }
